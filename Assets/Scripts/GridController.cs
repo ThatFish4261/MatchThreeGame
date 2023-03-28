1d00a8461271e5b97691a236e64e32c3dd314d9e
ThatFish4261 <75697596+ThatFish4261@users.noreply.github.com>
2023-03-27 20:51:38 -0700

    Added random generation and a way to track mouse inputs

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -5,19 +5,52 @@ using UnityEngine;
 public class GridController : MonoBehaviour
 {
 
-    private int[,] grid = new int[8, 8];
+    [SerializeField] private GameObject piecePrefab1;
+    [SerializeField] private GameObject piecePrefab2;
+    [SerializeField] private GameObject piecePrefab3;
+
+
+    [SerializeField] private Vector3 originPosition;
+
+    private Piece[,] grid = new Piece[8, 8];
+
+    // [[0, 1, 2, 3, 4], [], [], []]
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(grid); 
+        // Debug.Log(grid);
 
-        for (int row = 0; row < grid.Length; row++)
+        for (int row = 0; row < grid.GetLength(0); row++)
         {
             for (int column = 0; column < grid.GetLength(1); column++)
             {
-                grid[row, column] = column;
-                Debug.Log(grid[row, column]);
+                System.Random rand = new System.Random();
+                int randomNumber = rand.Next(1,30);
+
+                Vector3 newWorldPosition = new Vector3(originPosition.x + row, originPosition.y, originPosition.z - column);
+
+                grid[row, column] = new Piece(newWorldPosition, new Vector2(row, column));
+                
+                if (randomNumber >= 1 && randomNumber < 10)
+                {
+                    Instantiate(piecePrefab1, grid[row, column].GetPosition(), Quaternion.identity);
+                }
+
+                else if (randomNumber >= 11 && randomNumber < 20)
+                {
+                    Instantiate(piecePrefab2, grid[row, column].GetPosition(), Quaternion.identity);
+                }
+
+                else if (randomNumber >= 21 && randomNumber < 30)
+                {
+                    Instantiate(piecePrefab3, grid[row, column].GetPosition(), Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(piecePrefab1, grid[row, column].GetPosition(), Quaternion.identity);
+                }
+
             }
         }
     }
@@ -25,6 +58,9 @@ public class GridController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        /*if (Input.GetMouseButtonDown(0))
+        {
+            Destroy
+        }*/
     }
 }
--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -2,24 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Piece : MonoBehaviour
+public enum PieceTypes
 {
+    Deborah = 0,
+    Elisha = 1,
+    Lamb = 2,
+    Andrew = 3
+}
+
+public class Piece
+{
+    // This script will manage the state of a single
+    // piece in the grid.
 
     private Vector3 position;
     private Vector2 gridPosition;
-    private GameObject piecePrefab;
+    private PieceTypes pieceType;
 
-    // Start is called before the first frame update
     public Piece()
     {
         position = Vector3.zero;
         gridPosition = Vector2.zero;
+        pieceType = PieceTypes.Andrew;
     }
 
-    public Piece(Vector3 position, Vector2 gridPosition, GameObject piecePrefab)
+    public Piece(Vector3 position, Vector2 gridPosition)
     {
         this.position = position;
         this.gridPosition = gridPosition;
-        this.piecePrefab = piecePrefab;
+        this.pieceType = PieceTypes.Andrew;
+    }
+
+    public Piece(Vector3 position, Vector2 gridPosition, PieceTypes pieceType)
+    {
+        this.position = position;
+        this.gridPosition = gridPosition;
+        this.pieceType = pieceType;
+    }
+
+    public Vector3 GetPosition()
+    {
+        return position;
+    }
+
+    public PieceTypes GetPieceType()
+    {
+        return pieceType;
     }
 }
