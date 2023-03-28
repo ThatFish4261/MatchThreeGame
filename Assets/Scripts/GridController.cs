7a81a0b55db58b0f70b2ca622eddf62c12f9388e
ThatFish4261 <75697596+ThatFish4261@users.noreply.github.com>
2023-03-28 12:51:59 -0700

    Updated Random Generation so 3 of the same pieces don't spawn above or below each other

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -9,7 +9,6 @@ public class GridController : MonoBehaviour
     [SerializeField] private GameObject piecePrefab2;
     [SerializeField] private GameObject piecePrefab3;
 
-
     [SerializeField] private Vector3 originPosition;
 
     private Piece[,] grid = new Piece[8, 8];
@@ -19,38 +18,51 @@ public class GridController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Debug.Log(grid);
-
         for (int row = 0; row < grid.GetLength(0); row++)
         {
+            // Store the previous piece's type to avoid spawning the same type above/below
+            int previousPieceType = -1;
+
+            // Initialize a list of available piece types
+            List<int> availablePieceTypes = new List<int> { 1, 2, 3 };
+
             for (int column = 0; column < grid.GetLength(1); column++)
             {
-                System.Random rand = new System.Random();
-                int randomNumber = rand.Next(1,30);
+                // Remove the previous piece's type from the available list if it exists
+                if (previousPieceType != -1)
+                {
+                    availablePieceTypes.Remove(previousPieceType);
+                }
+
+                // Spawn a random piece from the available list
+                int randomIndex = Random.Range(0, availablePieceTypes.Count);
+                int pieceType = availablePieceTypes[randomIndex];
 
                 Vector3 newWorldPosition = new Vector3(originPosition.x + row, originPosition.y, originPosition.z - column);
 
                 grid[row, column] = new Piece(newWorldPosition, new Vector2(row, column));
-                
-                if (randomNumber >= 1 && randomNumber < 10)
+
+                if (pieceType == 1)
                 {
                     Instantiate(piecePrefab1, grid[row, column].GetPosition(), Quaternion.identity);
                 }
-
-                else if (randomNumber >= 11 && randomNumber < 20)
+                else if (pieceType == 2)
                 {
                     Instantiate(piecePrefab2, grid[row, column].GetPosition(), Quaternion.identity);
                 }
-
-                else if (randomNumber >= 21 && randomNumber < 30)
+                else
                 {
                     Instantiate(piecePrefab3, grid[row, column].GetPosition(), Quaternion.identity);
                 }
-                else
-                {
-                    Instantiate(piecePrefab1, grid[row, column].GetPosition(), Quaternion.identity);
-                }
 
+                // Update the previous piece's type
+                previousPieceType = pieceType;
+
+                // Add the next piece type to the available list
+                if (availablePieceTypes.Count < 3)
+                {
+                    availablePieceTypes.Add(previousPieceType % 3 + 1);
+                }
             }
         }
     }
@@ -58,9 +70,6 @@ public class GridController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*if (Input.GetMouseButtonDown(0))
-        {
-            Destroy
-        }*/
+        
     }
 }
